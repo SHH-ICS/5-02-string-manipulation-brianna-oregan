fa24995655ba117c1971552dbf35c40a7b45523d
Brianna O'Regan <brianna.oregan@stu.ocsb.ca>
2024-11-26 15:30:54 +0000

    add Q2Reverse.py

--- Q2Reverse.py
+++ Q2Reverse.py
@@ -1,11 +1,5 @@
 # Create a program that will accept a word and output the word one letter at a time in reverse.
-#get user input for the word
 word = input("Enter a word: ")
 
-# initialize empty strign to build the triangle
-current = ""
-
-#build the word triangle
-for char in word:
-    current += char #adds 1 character to current at a time
-    print(current)
+for char in reversed(word):
+    print(char)
